6035992fd6ab81982eb890cd2dbb3982bd2852d9
seoyon <fler9617@kaist.ac.kr>
2015-10-30 01:53:19 -0700

    models & syncdb & template syntax

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from app.models import *
+
 # Create your views here.
 
 def hello_world(request):
     return render(request, 'hello_world.html')
+
+def main_page(request):
+    comments = Comment.objects.order_by('date')
+    var = {
+        'comments': comments,
+    }
+    return render(request, 'home.html', var)
