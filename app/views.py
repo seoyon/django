83b9c8788447f84def363abaa896a68b80ed81bf
seoyon <fler9617@kaist.ac.kr>
2015-10-30 00:59:51 -0700

    hello_world view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello_world(request):
+    html = "<html><body><h1>Hello world!</h1></body></html>"
+    return HttpResponse(html)
--- project/urls.py~
+++ project/urls.py~
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', hello_world),
+    url(r'^$', main_page),
 )
